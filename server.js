df739951140f31284803bad57db7b0b062b96785
Matt Larsen <matthew.larsen13@gmail.com>
2024-11-15 23:28:14 -0500

    Nov 15

--- server.js
+++ server.js
@@ -1,138 +1,134 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const Joi = require("joi");
 app.use(cors());
 app.use(express.static("public"));
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "./public/images/");
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname);
-    },
+  destination: (req, file, cb) => {
+    cb(null, "./public/images/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 
 const upload = multer({ storage: storage });
 
 const housePlans = [
-{
-        "name": "Farmhouse",
-        "size": 2000,
-        "bedrooms": 3,
-        "bathrooms": 2.5,
-        "features": [
-            "wrap around porch",
-            "attached garage"
-        ],
-        "main_image": "farm.webp",
-        "floor_plans": [
-            {
-                "name": "Main Level",
-                "image": "farm-floor1.webp"
-            },
-            {
-                "name": "Basement",
-                "image": "farm-floor2.webp"
-            }
-        ]
-},
-{
-    "name": "Mountain House",
-    "size": 1700,
-    "bedrooms": 3,
-    "bathrooms": 2,
-    "features": [
-        "grand porch",
-        "covered deck"
+  {
+    _id: 1,
+    name: "Farmhouse",
+    size: 2000,
+    bedrooms: 3,
+    bathrooms: 2.5,
+    features: ["wrap around porch", "attached garage"],
+    main_image: "farm.webp",
+    floor_plans: [
+      {
+        name: "Main Level",
+        image: "farm-floor1.webp",
+      },
+      {
+        name: "Basement",
+        image: "farm-floor2.webp",
+      },
     ],
-    "main_image": "mountain-house.webp",
-    "floor_plans": [
-        {
-            "name": "Main Level",
-            "image": "mountain-house1.webp"
-        },
-        {
-            "name": "Optional Lower Level",
-            "image": "mountain-house2.webp"
-        },
-        {
-            "name": "Main Level Slab Option",
-            "image": "mountain-house3.jpg"
-        }
-    ]
-},
-{
-    "name": "Lake House",
-    "size": 3000,
-    "bedrooms": 4,
-    "bathrooms": 3,
-    "features": [
-        "covered deck",
-        "outdoor kitchen",
-        "pool house"
+  },
+  {
+    _id: 2,
+    name: "Mountain House",
+    size: 1700,
+    bedrooms: 3,
+    bathrooms: 2,
+    features: ["grand porch", "covered deck"],
+    main_image: "mountain-house.webp",
+    floor_plans: [
+      {
+        name: "Main Level",
+        image: "mountain-house1.webp",
+      },
+      {
+        name: "Optional Lower Level",
+        image: "mountain-house2.webp",
+      },
+      {
+        name: "Main Level Slab Option",
+        image: "mountain-house3.jpg",
+      },
     ],
-    "main_image": "farm.webp",
-    "floor_plans": [
-        {
-            "name": "Main Level",
-            "image": "lake-house1.webp"
-        },
-        {
-            "name": "Lower Level",
-            "image": "lake-house2.webp"
-        }
-    ]
-}];
+  },
+  {
+    _id: 3,
+    name: "Lake House",
+    size: 3000,
+    bedrooms: 4,
+    bathrooms: 3,
+    features: ["covered deck", "outdoor kitchen", "pool house"],
+    main_image: "farm.webp",
+    floor_plans: [
+      {
+        name: "Main Level",
+        image: "lake-house1.webp",
+      },
+      {
+        name: "Lower Level",
+        image: "lake-house2.webp",
+      },
+    ],
+  },
+];
 
-app.get("/",(req, res)=>{
-    console.log("getting me");
-    res.sendFile(_dirname + "/index.html");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/api/house_plans", (req, res)=>{
-    res.json(housePlans);
+app.get("/api/house_plans", (req, res) => {
+  res.json(housePlans);
 });
 
 app.post("/api/house_plans", upload.single("img"), (req, res) => {
-    console.log("In a post request");
+  console.log("In a post request");
 
-    const result = validateHouse(req.body);
+  const result = validateHouse(req.body);
 
-    if(result.error) {
-        res.status(400).send(result.error.details[0].message);
-        console.log("Error");
-        return;
-    }
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    console.log("I have an error");
+    return;
+  }
 
-    const house = {
-        name:req.body.name,
-        size:req.body.size,
-        bedrooms:req.body.bedrooms,
-        bathrooms:req.body.bathrooms
-    }
+  const house = {
+    _id: housePlans.length + 1,
+    name: req.body.name,
+    size: req.body.size,
+    bedrooms: req.body.bedrooms,
+    bathrooms: req.body.bathrooms,
+  };
 
-    if(req.file) {
-        house.main_image = req.file.filename;
-    }
+  if (req.file) {
+    house.main_image = req.file.filename;
+  }
 
-    housePlans.push(house);
+  housePlans.push(house);
 
-    console.log("WOW");
+  console.log(house);
+  res.status(200).send(house);
 });
 
 const validateHouse = (house) => {
-    const schema = Joi.object({
-        name:Joi.string().min(3).required(),
-        size:Joi.number().required(),
-        bedrooms:Joi.number().required(),
-        bathrooms:Joi.number().required()
-    });
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+    size: Joi.number().required(),
+    bedrooms: Joi.number().required(),
+    bathrooms: Joi.number().required(),
+  });
 
-    return schema.validate(house);
+  return schema.validate(house);
 };
 
 app.listen(3001, () => {
-    console.log("Listening...");
+  console.log("Listening....");
 });
